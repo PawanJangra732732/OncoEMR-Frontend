1dc6fa2682195206686dbecec858bd8cedb05561
templar-ajay <templar.ajay@gmail.com>
2024-07-15 12:55:43 +0530

    fix;

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,5 +1,1 @@
-const x = process.env.BACKEND_URL;
-if (!x) {
-  throw new Error(`no BACKEND_URL specified in the file ".env.local" `);
-}
-export const BACKEND_URL = x;
+export const BACKEND_URL = process.env.BACKEND_URL;
